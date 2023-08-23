fbc1716e2af3345bd9196c3741a088364c7e6822
Timofey <110742885+Timka111@users.noreply.github.com>
2023-08-23 23:34:05 +0300

    fix: fixed card

--- sckript/search.js
+++ sckript/search.js
@@ -1,17 +1,36 @@
-document.querySelector(".input__section").oninput = function () {
-  let val = this.value.trim();
-  let elastik = document.querySelectorAll(".vibot__item a");
-  if (val != "") {
-    elastik.forEach(function (elem) {
-      if (elem.innerText.search(val) == -1) {
-        elem.classlist.add("hide1");
-      } else {
-        elem.classlist.remove("hide1");
-      }
-    });
-  } else {
-    elastik.forEach(function (elem) {
-      elem.classlist.remove("hide1");
-    });
-  }
+window.onload = () => {
+  let input = document.querySelector("#tupeinput");
+  input.oninput = function () {
+    let value = this.value.trim();
+    let list = document.querySelectorAll(".card");
+
+    if (value != "") {
+      list.forEach((elem) => {
+        if (elem.innerText.search(value) == -1) {
+          elem.classList.add("hide1");
+          document.querySelector(".card").classList.add("hide1");
+        }
+      });
+    } else {
+      list.forEach((elem) => {
+        elem.classList.remove("hide1");
+        document.querySelector(".card").classList.remove("hide1");
+      });
+    }
+  };
 };
+
+const filterBox = document.querySelectorAll(".card");
+
+document.querySelector(".ul").addEventListener("click", (event) => {
+  if (event.target.tagName !== "LI") return false;
+
+  let filterClass = event.target.dataset["f"];
+
+  filterBox.forEach((elem) => {
+    elem.classList.remove("hide2");
+    if (!elem.classList.contains(filterClass) && filterClass !== "all") {
+      elem.classList.add("hide2");
+    }
+  });
+});
